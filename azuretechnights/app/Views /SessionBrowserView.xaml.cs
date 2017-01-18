342df0d505bb12817d451aee6e04240c76045285
William S. Rodriguez <williamsrz@outlook.com>
2017-01-18 21:42:33 -0200

    open session (webview)

---
+++ azuretechnights/app/Views /SessionBrowserView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using AzureTechNights.Models;
+using Xamarin.Forms;
+
+namespace AzureTechNights.Views
+{
+    public partial class SessionBrowserView : ContentPage
+    {
+        public SessionBrowserView(Session session)
+        {
+            InitializeComponent();
+
+            Title = session.Title;
+            webView.Source = session.Url;
+        }
+    }
+}
--- azuretechnights/app/Views /SessionsView.xaml.cs
+++ azuretechnights/app/Views /SessionsView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AzureTechNights.Models;
 using AzureTechNights.ViewModels;
 using Xamarin.Forms;
 
@@ -15,12 +16,26 @@ namespace AzureTechNights.Views
 
             ViewModel = new SessionsViewModel();
             BindingContext = ViewModel;
+
+            sessionsListView.ItemSelected += SessionsListViewItemSelected;
         }
 
         protected override async void OnAppearing()
         {
             ViewModel.RefreshSessionCommand.Execute(null);
             base.OnAppearing();
+        }
+
+        private async void SessionsListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var session = e.SelectedItem as Session;
+
+            if (session == null)
+                return;
+
+            await Navigation.PushAsync(new SessionBrowserView(session));
+
+            sessionsListView.SelectedItem = null;
         }
 
     }
