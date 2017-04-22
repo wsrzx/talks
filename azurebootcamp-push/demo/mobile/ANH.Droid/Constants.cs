9de93fda4b5e8002be8cbc223630f2b2ef28089d
William S. Rodriguez <williamsrz@outlook.com>
2017-04-22 15:03:05 -0300

    demo azure bootcamp

---
+++ azurebootcamp-push/demo/mobile/ANH.Droid/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ANH.Droid
+{
+    public static class Constants
+    {
+        public const string SenderID = "334092605365"; // Google API Project Number
+        public const string ListenConnectionString = "Endpoint=sb://nogare-ns.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=sMSfLUinhnNMItO+wSOL3jv3CEBt2E294nuvj05KA3Y="; // 
+        public const string NotificationHubName = "nogare-hub"; // 
+    }
+}
