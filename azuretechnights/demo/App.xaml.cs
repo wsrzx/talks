31d04e88ec7a056965a0f89190cc825c744171e1
William S. Rodriguez <williamsrz@outlook.com>
2017-07-26 08:31:56 -0300

    update dir

---
+++ azuretechnights/demo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using AzureTechNights.Views;
+
+namespace AzureTechNights
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var content = new SessionsView();
+            MainPage = new NavigationPage(content);
+        }
+    }
+}
